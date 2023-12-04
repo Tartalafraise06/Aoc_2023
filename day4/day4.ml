c4598ecf91bfd833e0eb0331d2f872986d22f3e9
Tarte <lucasguidet06@gmail.com>
2023-12-04 06:58:24 +0100

    day4*

--- day4/day4.ml
+++ day4/day4.ml
@@ -41,6 +41,21 @@ let rec print_list =
 
 
 let res tab = 
+  let c = ref 0 in
+  for i = 0 to Array.length tab -1 do 
+    let nb_g = ref 0 in 
+    let l1 = List.nth (String.split_on_char ':' tab.(i))  1 in
+    let l = String.split_on_char '|'l1 in
+    let [lEnters;lNum] = List.map (fun a -> String.split_on_char ' ' a) l in
+    List.iter (fun a ->  if a = "" then () else if List.mem a lEnters then begin
+      if !nb_g = 0 then (nb_g := 1;)
+                                                  else nb_g := !nb_g*2 end ) lNum;
+    c := !c + (!nb_g);                                
+  done;
+  !c
+
+
+let res2 tab = 
   let c = ref (Array.length tab) in
   let scratch = Array.make (Array.length tab) 1 in 
 
@@ -61,14 +76,15 @@ let res tab =
   !c
 
 
+
 let () = 
   let fichier = read_lines (open_in "day4.txt") in
   let tab =  Array.of_list fichier in
   let r1 = res tab in 
-  (*let r2 = res2 tab in *)
+  let r2 = res2 tab in 
   print_string "\n \n";
   print_string "le résultat est : ";
   print_int (r1);
   print_string "\n \n";
   print_string "le résultat 2 est : ";
-  (*print_int (r2);*)
+  print_int (r2);
